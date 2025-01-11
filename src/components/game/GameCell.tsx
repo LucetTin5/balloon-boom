7cf8cb1245ddfd393378cb27a6e0cfbe1705c9ab
LucetTin5 <lucettin5@gmail.com>
2025-01-11 20:48:03 +0900

    asset: add balloon image for balloon cell

--- src/components/game/GameCell.tsx
+++ src/components/game/GameCell.tsx
@@ -6,7 +6,9 @@ interface GameCellProps {
 export const GameCell = ({ isActive, onClick }: GameCellProps) => (
   <div
     className={`min-w-[80px] min-h-[80px] border aspect-square w-full ${
-      isActive ? 'bg-teal-500' : ''
+      isActive
+        ? 'bg-teal-700 bg-[url("/balloon.png")] bg-center bg-no-repeat bg-clip-content'
+        : ''
     }`}
     onClick={onClick}
   />
