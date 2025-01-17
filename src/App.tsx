3a83978acddb48cc39c71bc0ad8ef78430cc2c70
LucetTin5 <lucettin5@gmail.com>
2025-01-17 11:10:03 +0900

    fix: use tailwind safelist to remove inline-style

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ const App = () => {
         <div className="w-full flex flex-col items-center justify-center relative">
           <GameBoard
             board={board}
+            boardWidth={boardWidth}
             status={status}
             onClickBalloon={handleClickBalloon}
           />
--- src/components/game/GameBoard.tsx
+++ src/components/game/GameBoard.tsx
@@ -5,20 +5,17 @@ import { GameStatus } from '@/game/types';
 
 interface GameBoardProps {
   board: boolean[][];
+  boardWidth: number;
   status: GameStatus;
   onClickBalloon: (row: number, col: number) => void;
 }
 
 export const GameBoard = memo(
-  ({ board, status, onClickBalloon }: GameBoardProps) => {
+  ({ board, boardWidth, status, onClickBalloon }: GameBoardProps) => {
     return (
       <div className="w-full max-w-[800px] aspect-square p-4 sm:p-10 flex flex-col items-center justify-center">
         <div
-          className={`w-full h-full grid ${status !== GameStatus.PLAYING ? 'cursor-not-allowed blur-md' : ''}`}
-          style={{
-            gridTemplateColumns: `repeat(${board.length}, 1fr)`,
-            gridTemplateRows: `repeat(${board.length}, 1fr)`,
-          }}
+          className={`w-full h-full grid grid-cols-${boardWidth} grid-rows-${boardWidth} ${status !== GameStatus.PLAYING ? 'cursor-not-allowed blur-md' : ''}`}
         >
           {board.map((row, rowIndex) =>
             row.map((cell, colIndex) => (
